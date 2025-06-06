90a2cc01dd0343aec72f71debecbc9dc42dd45ff
Sakshi Bansal <sakshibansal@Sakshis-MacBook-Pro.local>
2025-06-06 15:40:22 +0530

    Add home page for library system

---
+++ src/app/ApolloWrapper.jsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { HttpLink } from "@apollo/client";
+import {
+  ApolloNextAppProvider,
+  ApolloClient,
+  InMemoryCache,
+} from "@apollo/client-integration-nextjs";
+
+function makeClient() {
+  const httpLink = new HttpLink({
+    uri: "http://localhost:3000/api/graphql",
+    fetchOptions: {
+    },
+  });
+
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+    link: httpLink,
+  });
+}
+
+export function ApolloWrapper({ children }) {
+  return (
+    <ApolloNextAppProvider makeClient={makeClient}>
+      {children}
+    </ApolloNextAppProvider>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,45 @@
+"use client";
+
+import Link from 'next/link';
+import Image from "next/image";
+import { useQuery } from '@apollo/client';
+import { GET_BOOKS } from '../../lib/queries';
+import BookCard from './components/BookCard';
+
+
+export default function Home() {
+  const { loading, error, data } = useQuery(GET_BOOKS);
+
+  return (
+    <div>
+      <div className="mb-8">
+        <h1 className="text-3xl font-bold text-gray-900 text-center py-12">Library</h1>
+        <h2 className="text-3xl font-bold text-gray-900 text-center py-12 mt-2 text-gray-600"> Discover and manage your favorite books </h2>
+      </div>
+
+      <div className="items-center justify-items-center min-h-screen p-8 pb-2 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-[1px] row-start-2 items-center sm:items-start">
+          <div className="flex gap-4 items-center flex-col sm:flex-row">
+            <div>
+              <Link
+                href={`/books`}
+                className="flex-1 bg-blue-600 text-white text-center py-2 px-3 rounded-md text-sm font-medium hover:bg-blue-700 transition-colors"
+              >
+                View Books
+              </Link>
+            </div>
+            <div>
+              <Link
+                href={`/authors`}
+                className="flex-1 bg-blue-600 text-white text-center py-2 px-3 rounded-md text-sm font-medium hover:bg-blue-700 transition-colors"
+              >
+                View Authors
+              </Link>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
+
