84726950df0002639d6fd65dacce9cdbbad44ace
Sakshi Bansal <sakshi.april5@gmail.com>
2025-06-09 12:30:44 +0530

    Fix graphql server link

--- src/app/ApolloWrapper.jsx
+++ src/app/ApolloWrapper.jsx
@@ -1,29 +1,24 @@
 "use client";
 
-import { HttpLink } from "@apollo/client";
-import {
-  ApolloNextAppProvider,
-  ApolloClient,
-  InMemoryCache,
-} from "@apollo/client-integration-nextjs";
+import {HttpLink} from "@apollo/client";
+import {ApolloClient, ApolloNextAppProvider, InMemoryCache,} from "@apollo/client-integration-nextjs";
 
 function makeClient() {
-  const httpLink = new HttpLink({
-    uri: "http://localhost:3000/api/graphql",
-    fetchOptions: {
-    },
-  });
+    const httpLink = new HttpLink({
+        uri: "/api/graphql",
+        fetchOptions: {},
+    });
 
-  return new ApolloClient({
-    cache: new InMemoryCache(),
-    link: httpLink,
-  });
+    return new ApolloClient({
+        cache: new InMemoryCache(),
+        link: httpLink,
+    });
 }
 
-export function ApolloWrapper({ children }) {
-  return (
-    <ApolloNextAppProvider makeClient={makeClient}>
-      {children}
-    </ApolloNextAppProvider>
-  );
+export function ApolloWrapper({children}) {
+    return (
+        <ApolloNextAppProvider makeClient={makeClient}>
+            {children}
+        </ApolloNextAppProvider>
+    );
 }
