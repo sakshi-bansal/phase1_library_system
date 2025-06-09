cb5402b8a1add01640a06a8cc5d61dd82f08cade
Sakshi Bansal <sakshi.april5@gmail.com>
2025-06-09 12:40:04 +0530

    Updating config to support production config

--- config/config.js
+++ config/config.js
@@ -9,5 +9,13 @@ module.exports = {
     dialect: process.env.DB_DIALECT,
     port: process.env.DB_PORT,
   },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
+  },
 };
 
